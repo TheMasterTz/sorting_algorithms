f6e333384f0682738dcfbd245ff3b0242e0bd5b5
TheMasterTz <palavras2001@hotmail.com>
2021-10-20 19:43:36 -0500

    add the file 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,27 +10,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, n, new_n;
-	int swap;
+	size_t n, n2;
+	int iter;
 
-	if (array == NULL || size < 2)
+	if (array == NULL)
 		return;
 
 	n = size;
 	while (n > 0)
 	{
-		new_n = 0;
-		for (i = 0; i < n - 1; i++)
+		n2 = 0;
+		for (iter = 0; iter < n; iter++)
 		{
-			if (array[i] > array[i + 1])
-			{
-				swap = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = swap;
-				new_n = i + 1;
-				print_array(array, size);
-			}
+			
 		}
-		n = new_n;
 	}
 }
